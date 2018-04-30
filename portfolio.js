86b71be375b9af869d2516390937c1221ceb5b29
Rubaiyat Noman <rf.noman@yahoo.com>
2018-05-01 00:51:29 +0600

    mobile

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,56 @@
 var selected;
+
 $(document).ready(function () {
+
     var temp=1;
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        $("#phone").show();
+        $(".box").css("display","none");
+        $("#home").hide();
+        $(".page").css("height","96vh");
+
+    }
+
+    else
+    {
+        $("#pc").show();
+    }
+    $(".phone-menu-item").click(function(){
+
+        var select = $(this).children().attr("id");
+        if (select === "point3"||select==="phone-pic");
+        else
+        {
+
+            if (temp === 1) {
+                animation();
+                temp = 0;
+
+            }
+            pages(select);
+        }
+    });
+    $(".phone-box").click(function(){
+
+        var select = $(this).children().attr("id");
+        if (select === "point3"||select==="phone-pic");
+        else
+        {
+
+            if (temp === 1) {
+                animation();
+                temp = 0;
+
+            }
+            pages(select);
+        }
+
+    });
     $(".box").click(function () {
         $(".points").css({"background-color": "#ebebeb", "color": "grey"});
         $(this).children().css({"background-color": "grey", "color": "black"});
         var select = $(this).children().attr("id");
-        if (select === "point3");
+        if (select === "point3"||select==="phone-pic");
         else
         {
 
@@ -19,6 +64,7 @@ $(document).ready(function () {
     });
 
     $("#home").click(function () {
+
         $(".home-box").fadeOut(500);
         $(".page").hide(1500);
         $("#test").fadeIn(2000);
@@ -35,11 +81,25 @@ $(document).ready(function () {
         $(".header").hide(500);
         temp=1;
         $(selected).children().hide(500);
-    })
+    });
+
+
+    $("#phone-back").click(function (){
+        $(".page").fadeOut(1000).delay(1000);
+
+        $("#phone").show(1000);
+    });
+
+    $("#menu-image").click(function () {
+        $("#menu-bar").toggle(500);
+    });
+
+
 
 });
 function animation()
 {
+
     $("#test").hide(2000);
     $(".profile-pic").animate({"width":"2vw","height":"2vw"},2000);
     $(".box").animate({"margin-top":"0vh",
@@ -54,7 +114,21 @@ function animation()
 
 }
 function pages(select) {
-    selected="#"+select+"i";
+    if(select.match("phone"))
+    {
+        select=select.replace("-phone","");
+        $("#phone-top").show(2500);
+        $("#pc-header").hide();
+        $(".page").show(2000);
+    }
+    else if(select.match("menu"))
+    {
+        select=select.replace("-menu","");
+        $("#menu-bar").hide(500);
+    }
+
+    selected = "#" + select + "i";
+    $(".phone").fadeOut(500).delay(1000);
     $(selected).show(1000);
     $(selected).children().fadeIn(4000);
     $(selected).siblings().hide(1000);
