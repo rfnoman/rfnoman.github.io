4726fc310095e91f959ed7662149ab493092aed0
Rubaiyat Noman <rf.noman@yahoo.com>
2018-05-01 01:58:29 +0600

    bug

--- portfolio.js
+++ portfolio.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
     });
 
     $("#home").click(function () {
-
+		$("#menu-bar").hide(500);
         $(".home-box").fadeOut(500);
         $(".page").hide(1500);
         $("#test").fadeIn(2000);
